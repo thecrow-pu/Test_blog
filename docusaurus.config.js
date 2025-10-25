a5b4ca79d78fbdc95d93c44583fb540df4ac5215
thecrow-pu <huathanhhuy2018.tn@gmail.com>
2025-10-25 12:20:13 +0700

    3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: "https://thecrow-pu.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/thecrow-pu/",
+  baseUrl: "/Test-blog/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
